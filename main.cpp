fd64fce8a0b3e3052e8e58e896c2fc2bf240e760
wongkokseng <1221206470@student.mmu.edu.my>
2026-07-02 22:49:00 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,41 +1,51 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <iomanip>
-#include <sstream>
+// Add these methods inside your Database class
 
-using namespace std;
+static void initializeMenu() {
+    ifstream file("menu.txt");
+    if (!file.is_open()) {
+        ofstream newFile("menu.txt");
+        newFile << "1,Cheeseburger,5.99\n";
+        newFile << "2,Pizza Margherita,8.50\n";
+        newFile << "3,Caesar Salad,4.75\n";
+        newFile << "4,French Fries,2.99\n";
+        newFile << "5,Soda,1.50\n";
+        newFile.close();
+    }
+}
 
-// --- Data Structures ---
-struct MenuItem {
-    int id;
-    string name;
-    double price;
-};
-
-struct CartItem {
-    MenuItem item;
-    int quantity;
-};
-
-// --- Database Simulation (File Handling) ---
-class Database {
-public:
-    // Methods will be implemented in Stage 2
-};
-
-// --- Application Logic ---
-class FoodApp {
-private:
-    string currentUser;
+static vector<MenuItem> loadMenu() {
     vector<MenuItem> menu;
-    vector<CartItem> cart;
+    ifstream file("menu.txt");
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string idStr, name, priceStr;
+        getline(ss, idStr, ',');
+        getline(ss, name, ',');
+        getline(ss, priceStr, ',');
+        menu.push_back({stoi(idStr), name, stod(priceStr)});
+    }
+    return menu;
+}
 
-public:
-    // Methods will be implemented in Stages 3-5
-};
+static bool registerUser(const string& username, const string& password) {
+    ifstream inFile("users.txt");
+    string u, p;
+    while (inFile >> u >> p) {
+        if (u == username) return false; 
+    }
+    inFile.close();
 
-int main() {
-    return 0;
+    ofstream outFile("users.txt", ios::app);
+    outFile << username << " " << password << "\n";
+    return true;
+}
+
+static bool loginUser(const string& username, const string& password) {
+    ifstream file("users.txt");
+    string u, p;
+    while (file >> u >> p) {
+        if (u == username && p == password) return true;
+    }
+    return false;
 }
